3ba4c0fe5e2461f3626e702956d2240ecd67d753
Scott A. Matthews <scott@designsbysm.com>
2022-04-20 17:06:14 -0700

    prebuild package

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+declare const _default: React.ComponentType<Pick<{
+    theme: ReactNativePaper.Theme;
+}, never> & {
+    theme?: import("@callstack/react-theme-provider").$DeepPartial<ReactNativePaper.Theme> | undefined;
+}> & import("@callstack/react-theme-provider/typings/hoist-non-react-statics").NonReactStatics<React.ComponentType<{
+    theme: ReactNativePaper.Theme;
+}> & React.FC<{}>, {}>;
+export default _default;
